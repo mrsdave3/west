f0229b92985a56d8b39e30f00b33afde51b422ac
mrsdave3 <mrsdave3@gmail.com>
2017-12-14 20:08:51 +0000

    Add files via upload

--- sermons3.php
+++ sermons3.php
@@ -9,20 +9,35 @@
 <body>
 <div class="wrapper">
 <?php include "fullHeader3.html";?>
-<div class="main">
-     <div class="col">5 November 2017 - Rev D J Kane (Communion)</div>
-     <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/5Nov17.mp3" type="audio/mpeg" /></audio></div>       
-        
-     <div class="col">29 October 2017 - Mr Chris Wilson</div>
-     <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/29Oct17.mp3" type="audio/mpeg"/></audio></div>
-      
-      
-     <div class="col">22 October 2017 - Men United Service</div>
-     <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/22Oct17.mp3" type="audio/mpeg"/></audio></div>
-</div>
-<div class="footer">
-<?php include 'footer3.php';?>      
-</div>
+    <div class="main">
+     <div class="col">10 December 2017 - Mr Chris Wilson</div>
+         <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/10Dec17.mp3" type="audio/mpeg" /></audio></div>  
+         
+    <div class="col">3 December 2017 - Rev D J Kane</div>
+         <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/3Dec17.mp3" type="audio/mpeg" /></audio></div>  
+         
+    <div class="col">26 November 2017 - Women's Ministry</div>
+         <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/26Nov17.mp3" type="audio/mpeg" /></audio></div>  
+    
+    <div class="col">19 November 2017 - GB Enrolment</div>
+         <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/19Nov17.mp3" type="audio/mpeg" /></audio></div>  
+         
+    <div class="col">12 November 2017 - Remembrance Service</div>
+         <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/12Nov17.mp3" type="audio/mpeg" /></audio></div>  
+         
+         <div class="col">5 November 2017 - Rev D J Kane (Communion)</div>
+         <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/5Nov17.mp3" type="audio/mpeg" /></audio></div>       
+            
+         <div class="col">29 October 2017 - Mr Chris Wilson</div>
+         <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/29Oct17.mp3" type="audio/mpeg"/></audio></div>
+          
+          
+         <div class="col">22 October 2017 - Men United Service</div>
+         <div class="music"><audio controls><source src="http://www.westchurchballymena.org/Sermons/22Oct17.mp3" type="audio/mpeg"/></audio></div>
+    </div>
+        <div class="footer">
+        <?php include 'footer3.php';?>      
+        </div>
 </div>
 </body>
 </html>
